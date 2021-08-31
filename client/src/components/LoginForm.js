2f52252a6c8c43c440a7cf5b63cadafbd5364a67
Hripsime Tepelikian <htepelikian@gmail.com>
2021-08-30 18:03:37 -0700

    mutation login user declared

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import {useMutation} from '@apollo/react-hooks';
 
-import { loginUser } from '../utils/API';
+
+import { loginUser } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, {error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
