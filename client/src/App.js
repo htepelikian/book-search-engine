00879153505ed3f62539a82961935f9d02ebc9a3
Hripsime Tepelikian <htepelikian@gmail.com>
2021-08-30 18:15:19 -0700

    connect apollo client attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {ApolloProvider} from '@apollo/react-hooks';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
